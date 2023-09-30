fb63bf525032fc3134f4ccb1ed8c4fcd10ed2947
emocreator <58938156+emocreator@users.noreply.github.com>
2023-09-30 20:22:46 +0100

    Uni project done

---
+++ Risc-V/Risc-V.srcs/sources_1/new/PC_Adder.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.09.2023 18:51:30
+// Design Name: 
+// Module Name: PC_Adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PC_Adder(
+
+    );
+endmodule
